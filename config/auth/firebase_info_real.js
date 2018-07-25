df4f3cfb10cc697aaa4f5e5479ad0f79e495e786
Matheus Rufca <matheus.rufca@gmail.com>
2018-07-25 17:09:41 -0300

    Set firebase config

--- config/auth/firebase_info_real.js
+++ config/auth/firebase_info_real.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "******************************",
-  authDomain: "**************.firebaseapp.com",
-  databaseURL: "https://*************.firebaseio.com",
-  projectId: "***************",
-  storageBucket: "",
-  messagingSenderId: "***************"
+  apiKey: "AIzaSyAoruLbzxYBLDGDh3jrv-MSw8MZ58y2pVE",
+  authDomain: "reactnative-poc.firebaseapp.com",
+  databaseURL: "https://reactnative-poc.firebaseio.com",
+  projectId: "reactnative-poc",
+  storageBucket: "reactnative-poc.appspot.com",
+  messagingSenderId: "285745774142"
 };
